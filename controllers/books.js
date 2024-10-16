7d8c1ae48d50f335d5ab27db75f245a1122db8b5
amandamigliorini <amanda.migliorini@hotmail.com>
2024-10-16 18:45:35 -0300

    books collection done

---
+++ controllers/books.js
@@ -0,0 +1,92 @@
+const Book = require('../middleware/bookSchema');
+
+const addBook = async (req, res) => {
+    //#swagger.tags=['Books']
+    try {
+        const newBook = new Book(req.body); 
+        // here is wher mongoose will make its validation
+        await newBook.save();
+        //succesfull message
+        return res.status(201).json({ message: 'Book added successfully!', book: newBook });
+    } catch (err) {
+        //error message (error handling)
+        return res.status(500).json({ message: 'Some error occured while adding the book', error: err.message });
+    }
+};
+
+const updateBook = async (req, res) =>{
+    //#swagger.tags=['Books']
+    try {
+        const updatedBook = await Book.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedBook) {
+            return res.status(404).json({
+                message: 'Book not found'
+            });
+        }
+
+        return res.status(200).json({
+            message: 'Book updated successfully!',
+            book: updatedBook
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            message: 'An error occured while updating the book',
+            error: err.message
+        });
+    }
+};
+
+const getAll = async (req, res) => {
+    //#swagger.tags=['Books']
+    try {
+        const books = await Book.find().exec(); 
+        res.status(200).json(books);
+    } catch (err) {
+        //error message (error handling)
+        return res.status(500).json({ message: 'Some error occured while getting the books', error: err.message });
+    }
+};
+
+const getSingle = async (req, res) => {
+    //#swagger.tags=['Books']
+    try {
+        const book = await Book.findById(req.params.id); 
+        if (!book) {
+            return res.status(404).json({ message: 'Book not found.' });
+        }
+        res.status(200).json(book);
+    } catch (err) {
+        //error message (error handling)
+        return res.status(500).json({ message: 'Some error occured while getting the book', error: err.message });
+    }
+};
+
+const deleteBook = async (req, res) => {
+    //#swagger.tags=['Books']
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id); 
+        if (!book) {
+            return res.status(404).json({ message: 'Book not found.' });
+        }
+        return res.status(200).json({ message: 'Book deleted'});
+    } catch (err) {
+        //error message (error handling)
+        return res.status(500).json({ message: 'Some error occured while deleting the book', error: err.message });
+    }
+};
+
+
+
+module.exports = {
+    addBook,
+    updateBook,
+    getAll,
+    getSingle,
+    deleteBook
+}
--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require("dotenv").config()
+const { connectDb } = require('./database/db.js');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+connectDb().then(() => {
+  console.log('Database connected successfully');
+}).catch(err => {
+  console.log('Failed to connect to the database', err.message);
+});
 
 app.use('/', require('./routes/main'));
 
