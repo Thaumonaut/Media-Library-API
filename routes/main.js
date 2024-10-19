23799b5e3b97d279dd3e9b5e6d27c6603012cc82
jpaulocts <jpaulocts@gmail.com>
2024-10-19 00:06:48 -0300

    paulo branch

--- routes/main.js
+++ routes/main.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 
 router.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swagggerDocument))
 
-router.use('/movies', require('./movies'))
+router.use('/movies', require('./movies'))//#swagger.tags=['Movies']
 
 router.use('/books', require('./books'))//#swagger.tags=['Books']
 
